7afcfd966075407dde58f6615a49b12a57f0de7d
minjae <minj219aa@gmail.com>
2022-01-01 22:56:00 +0900

    dockerfile

--- env.js
+++ env.js
@@ -1,1 +1,3 @@
-export const BACKEND_URI = 'http://localhost:8080'
+const ip = "http://13.125.30.195/"
+// export const BACKEND_URI = 'http://localhost:8080'
+export const BACKEND_URI = `http://${ip}:8080`
