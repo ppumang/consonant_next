b0299105c6523cb5aa26f2d099d50935dddf48ab
minjae <minj219aa@gmail.com>
2022-01-02 10:39:10 +0900

    제목변경

--- components/Logo.js
+++ components/Logo.js
@@ -3,8 +3,9 @@ const Logo = (props) => {
         <div style={{ display: 'flex', justifyContent: 'center' }}>
             <span style={{ fontFamily: 'dongle-bold', fontSize: 70, color: 'orange' }}>ㅊ</span>
             <span style={{ fontFamily: 'dongle-bold', fontSize: 70, color: 'white' }}>ㅅ</span>
-            <span style={{ fontFamily: 'dongle-bold', fontSize: 70, color: 'orange' }}>ㅍ</span>
-            <span style={{ fontFamily: 'dongle-bold', fontSize: 70, color: 'white' }}>ㅌ</span>
+            <span style={{ fontFamily: 'dongle-bold', fontSize: 70, color: 'orange' }}>ㅁ</span>
+            <span style={{ fontFamily: 'dongle-bold', fontSize: 70, color: 'white' }}>ㅊ</span>
+            <span style={{ fontFamily: 'dongle-bold', fontSize: 70, color: 'orange' }}>ㄱ</span>
         </div>
     )
 }
